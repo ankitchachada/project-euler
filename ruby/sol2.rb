4d3950973b62c7fcd871ecfc60bad13c702bcc99
ankit <ankit.chachada@gmail.com>
2016-11-09 01:59:43 -0800

    changes in sol2

--- ruby/sol2.rb
+++ ruby/sol2.rb
@@ -2,21 +2,22 @@ $a = []
 $a[0] = 0
 $a[1] = 1
 
-def av
-r = 0
-i = 0
-while i <= 40000
-  c = fib(i)
-  if c.even?
-  	r += c
+def sol2
+  r = 0
+  i = 0
+  while i <= 400
+    c = fib(i)
+    if c.even?
+  	  r += c
+    end
+    i += 1
+    break if c >= 4000000
   end
-  i += 1
-end
-puts r
+  puts r
 end
 
 def fib(n)
   $a[n] = $a[n-1] + $a[n-2]
 end
 
-av
+sol2
