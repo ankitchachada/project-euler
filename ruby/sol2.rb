1b6d01599a29a4f5f926ed153199afde2b11d70b
ankit <ankit.chachada@gmail.com>
2016-11-09 01:48:59 -0800

    sol2

--- ruby/sol2.rb
+++ ruby/sol2.rb
@@ -1,40 +1,22 @@
-# a = [] 
-# a[0] = 0 
-# a[1] = 1
+$a = []
+$a[0] = 0
+$a[1] = 1
 
-# i = 1
-# r = 0
-# while i <= 400
-# 	if i.even?
-#       a[i+1] = (a[i] + a[i-1])%1000000007
-#       r += a[i+1] 
-      
-#     end
-#     i += 1 
-# end
+def av
+r = 0
+i = 0
+while i <= 40000
+  c = fib(i)
+  if c.even?
+  	r += c
+  end
+  i += 1
+end
+puts r
+end
 
+def fib(n)
+  $a[n] = $a[n-1] + $a[n-2]
+end
 
-# puts r
-
-
-
-# $a = []
-# $a[0] = 0
-# $a[1] = 1
-
-# def av
-# r = 0
-# i = 0
-# while i <= 4000000
-#   c = fib(i)
-#   if c.even?
-#   	r += c
-#   end
-#   i += 1
-# end
-# puts r
-# end
-
-# def fib(n)
-#   $a[n] = $a[n-1] + $a[n-2]
-# end
+av
