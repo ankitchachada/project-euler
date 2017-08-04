3ff32596883b3a8c63b4cd60a8fa8e9d0eb92d67
ankit <ankit.chachada@gmail.com>
2017-08-04 10:48:48 -0400

    added question

--- ruby/sol1.rb
+++ ruby/sol1.rb
@@ -1,21 +1,16 @@
-#Multiples of 3 and 5
+# Question1:
+# If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+# Find the sum of all the multiples of 3 or 5 below 1000.
+
 puts "Euler project Solution 1"
 def find_sum()
-   s = 0
+   sum = 0
    (1...1000).map {|i|
-     s += i if i%3 == 0 || i%5 == 0
+     sum += i if i%3 == 0 || i%5 == 0
    }
-   p "The sum of multiples of 3 & 5 below 1000 is #{s}"
+   p "The sum of multiples of 3 & 5 below 1000 is #{sum}"
 end
 
-# def find_sum2()
-#    s = 0
-#    (1...1000).map {|i|
-#      s += i if i%3 == 0 || i%5 == 0
-#    }
-#    p "The sum of multiples of 3 & 5 below 1000 is #{s}"
-# end
-
 find_sum()
-# find_sum2()
 puts "End of Solution 1"
---
+++ ruby/sol8.rb
@@ -0,0 +1,4 @@
+#Largest product in a series
+def largest_project(num)
+	
+end
